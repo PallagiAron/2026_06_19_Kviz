de537e672c011225b4c2191895c09b4e228febc4
PallagiAron <pallagi.aron@diak.szbi-pg.hu>
2026-03-26 10:40:17 +0100

    teglalap adatainak betoltese

---
+++ value.js
@@ -0,0 +1,8 @@
+function szamol() {
+    console.log("hello")
+    let szelesseg = document.getElementById("szelesseg").value
+    let magassag = document.getElementById("magassag").value
+    
+    console.log(szelesseg)
+    console.log(magassag)
+}
